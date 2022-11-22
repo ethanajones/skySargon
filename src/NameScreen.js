c5a4def3ebc0930e7774b8d78de7847e2139c692
EAJ <eaj9990@gmail.com>
2022-11-22 01:57:21 -0800

    sargon can go home

--- src/NameScreen.js
+++ src/NameScreen.js
@@ -6,12 +6,21 @@ const NameScreen = () => {
         setName(e.target.value);
     }
 
+    return (<>
+        <h1>What is your name?</h1>
+        <input type="text" value={name} onChange={handleChange}/>
+        <NotSargon name={name}/>
+    </>)
+}
+
+const NotSargon = ({name}) => {
+
     return (
         <>
-            <h1>What is your name?</h1>
-            <input type="text" value={name} onChange={handleChange}/>
-            <h2>So you say your name is {name}</h2>
-        </>
-    )
+            {name ? <h2>So you say your name is {name}? </h2> : null}
+            {name !== "Sargon" ? <h3>Oh sorry, you're not the person I'm looking for.</h3> : <h3>Welcome Home.</h3>}
+        </>)
 }
+
+
 export default NameScreen;
